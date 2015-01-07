1f92a5ee738c7d4d8709f506e9645ea11f6cf179
Alexander Simonov <alex@simonov.me>
2015-01-07 14:04:04 -0800

    adding centos-7 support
    Signed-off-by: Sean OMeara <sean@chef.io>

--- attributes/rpmforge-extras.rb
+++ attributes/rpmforge-extras.rb
@@ -5,6 +5,8 @@ when 5
   default['yum']['rpmforge-extras']['mirrorlist'] = 'http://mirrorlist.repoforge.org/el5/mirrors-rpmforge-extras'
 when 6, 2013, 2014
   default['yum']['rpmforge-extras']['mirrorlist'] = 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-extras'
+when 7
+  default['yum']['rpmforge-extras']['mirrorlist'] = 'http://mirrorlist.repoforge.org/el7/mirrors-rpmforge-extras'
 end
 default['yum']['rpmforge-extras']['enabled'] = true
 default['yum']['rpmforge-extras']['managed'] = true
