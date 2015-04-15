7f9c8c0f3a79ce011aae964a8fe75f18d0bd1343
Sean OMeara <sean@chef.io>
2015-04-15 16:04:41 -0400

    adding urls to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'Sean OMeara <someara@chef.io>'
 license 'Apache 2.0'
 description 'Installs/Configures yum-repoforge'
 version '0.5.1'
+source_url 'https://github.com/chef-cookbooks/yum-repoforge'
+issues_url 'https://github.com/chef-cookbooks/yum-repoforge/issues'
 
 depends 'yum', '~> 3.0'
 depends 'yum-epel'
