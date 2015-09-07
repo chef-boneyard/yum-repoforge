2dd0e915fb657eddf6ae0971966e941d2b6dd077
Tim Smith <tsmith@chef.io>
2015-09-07 13:18:07 -0700

    Update the maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'yum-repoforge'
-maintainer 'Chef'
-maintainer_email 'Sean OMeara <someara@chef.io>'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-repoforge'
 version '0.5.3'
