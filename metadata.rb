486da3c3a5a7f6e7fac488374e6d457ac8a07aa5
Tim Smith <tsmith@chef.io>
2015-09-07 13:17:23 -0700

    Fix chef 11 compatibility

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'Sean OMeara <someara@chef.io>'
 license 'Apache 2.0'
 description 'Installs/Configures yum-repoforge'
 version '0.5.3'
-source_url 'https://github.com/chef-cookbooks/yum-repoforge'
-issues_url 'https://github.com/chef-cookbooks/yum-repoforge/issues'
+source_url 'https://github.com/chef-cookbooks/yum-repoforge'if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-repoforge/issues' if respond_to?(:issues_url)
 
 depends 'yum', '~> 3.2'
 depends 'yum-epel'
