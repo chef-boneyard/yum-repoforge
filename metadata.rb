28549c1747bda99fae974fe33ba211bacc77af62
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:03:54 -0700

    Add OS support and long_description

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,14 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-repoforge'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.5.3'
-source_url 'https://github.com/chef-cookbooks/yum-repoforge'if respond_to?(:source_url)
+source_url 'https://github.com/chef-cookbooks/yum-repoforge' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/yum-repoforge/issues' if respond_to?(:issues_url)
 
 depends 'yum', '~> 3.2'
 depends 'yum-epel'
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
