527593f0f5478f8aaf33f81fc9a76a7c5a3e7956
Sean OMeara <sean@chef.io>
2015-10-28 14:38:53 -0400

    Fixing Chef 13 nil property deprecation warnings

--- test/fixtures/cookbooks/yum-repoforge-wrapper/recipes/default.rb
+++ test/fixtures/cookbooks/yum-repoforge-wrapper/recipes/default.rb
@@ -1,8 +1,8 @@
-node.default['yum']['rpmforge']['enabled'] = false
-node.default['yum']['rpmforge']['managed'] = true
-node.default['yum']['rpmforge-extras']['enabled'] = false
-node.default['yum']['rpmforge-extras']['managed'] = true
-node.default['yum']['rpmforge-testing']['enabled'] = false
-node.default['yum']['rpmforge-testing']['managed'] = true
+node.override['yum']['rpmforge']['enabled'] = false
+node.override['yum']['rpmforge']['managed'] = true
+node.override['yum']['rpmforge-extras']['enabled'] = false
+node.override['yum']['rpmforge-extras']['managed'] = true
+node.override['yum']['rpmforge-testing']['enabled'] = false
+node.override['yum']['rpmforge-testing']['managed'] = true
 
 include_recipe 'yum-repoforge'
