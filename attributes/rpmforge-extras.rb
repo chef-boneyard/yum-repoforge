3f32740e464bdb045c51dccfd4e4b44bfc6ecacc
Jennifer Davis <iennae@gmail.com>
2016-08-18 22:57:11 -0700

    fixing cookstyle issues

--- attributes/rpmforge-extras.rb
+++ attributes/rpmforge-extras.rb
@@ -1,10 +1,6 @@
 default['yum']['rpmforge-extras']['repositoryid'] = 'rpmforge-extras'
 default['yum']['rpmforge-extras']['description'] = 'RHEL $releasever - RPMforge.net - extras'
-if node['platform'] == "amazon"
-  default['yum']['rpmforge-extras']['mirrorlist'] = 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-extras'
-else
-  default['yum']['rpmforge-extras']['mirrorlist'] = "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge-extras"
-end
+default['yum']['rpmforge-extras']['mirrorlist'] = (node['platform'] == 'amazon' ? 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-extras' : "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge-extras")
 default['yum']['rpmforge-extras']['enabled'] = true
 default['yum']['rpmforge-extras']['managed'] = true
 default['yum']['rpmforge-extras']['gpgcheck'] = true
--- attributes/rpmforge-testing.rb
+++ attributes/rpmforge-testing.rb
@@ -1,10 +1,6 @@
 default['yum']['rpmforge-testing']['repositoryid'] = 'rpmforge-testing'
 default['yum']['rpmforge-testing']['description'] = 'RHEL $releasever - RPMforge.net - testing'
-if node['platform'] == "amazon"
-  default['yum']['rpmforge-testing']['mirrorlist'] = 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-testing'
-else
-  default['yum']['rpmforge-testing']['mirrorlist'] = "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge-testing"
-end
+default['yum']['rpmforge-testing']['mirrorlist'] = (node['platform'] == 'amazon' ? 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-testing' : "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge-testing")
 default['yum']['rpmforge-testing']['enabled'] = true
 default['yum']['rpmforge-testing']['managed'] = false
 default['yum']['rpmforge-testing']['gpgcheck'] = true
