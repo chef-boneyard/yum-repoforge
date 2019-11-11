8a0e04b95f96dd1fb16f6f6af8a98ed43d663454
Tim Smith <tsmith84@gmail.com>
2019-11-10 18:08:36 -0800

    Use platform helpers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/rpmforge-extras.rb
+++ attributes/rpmforge-extras.rb
@@ -1,6 +1,6 @@
 default['yum']['rpmforge-extras']['repositoryid'] = 'rpmforge-extras'
 default['yum']['rpmforge-extras']['description'] = 'RHEL $releasever - RPMforge.net - extras'
-default['yum']['rpmforge-extras']['mirrorlist'] = (node['platform'] == 'amazon' ? 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-extras' : "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge-extras")
+default['yum']['rpmforge-extras']['mirrorlist'] = (platform?('amazon') ? 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-extras' : "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge-extras")
 default['yum']['rpmforge-extras']['enabled'] = true
 default['yum']['rpmforge-extras']['managed'] = true
 default['yum']['rpmforge-extras']['gpgcheck'] = true
--- attributes/rpmforge.rb
+++ attributes/rpmforge.rb
@@ -1,6 +1,6 @@
 default['yum']['rpmforge']['repositoryid'] = 'rpmforge'
 default['yum']['rpmforge']['description'] = 'RHEL $releasever - RPMforge.net - dag'
-default['yum']['rpmforge']['mirrorlist'] = (node['platform'] == 'amazon' ? 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge' : "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge")
+default['yum']['rpmforge']['mirrorlist'] = (platform?('amazon') ? 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge' : "http://mirrorlist.repoforge.org/el#{node['platform_version'].to_i}/mirrors-rpmforge")
 default['yum']['rpmforge']['enabled'] = true
 default['yum']['rpmforge']['managed'] = true
 default['yum']['rpmforge']['gpgcheck'] = true
